ceb28393a7813f4b7d97dbeeb5fe5d2fbca9737f
Viet Do <viet.do@trimble.com>
2020-06-23 22:12:09 +0300

    Part3 - StepVerifier done.

--- src/main/java/io/pivotal/literx/Part03StepVerifier.java
+++ src/main/java/io/pivotal/literx/Part03StepVerifier.java
@@ -16,10 +16,14 @@
 
 package io.pivotal.literx;
 
+import java.time.Duration;
 import java.util.function.Supplier;
 
 import io.pivotal.literx.domain.User;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Learn how to use StepVerifier to test Mono, Flux or any other kind of Reactive Streams Publisher.
@@ -33,14 +37,22 @@ public class Part03StepVerifier {
 
 	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then completes successfully.
 	void expectFooBarComplete(Flux<String> flux) {
-		fail();
+		StepVerifier.create(flux)
+				.expectNext("foo")
+				.expectNext("bar")
+				.expectComplete()
+				.verify();
 	}
 
 //========================================================================================
 
 	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then a RuntimeException error.
 	void expectFooBarError(Flux<String> flux) {
-		fail();
+		StepVerifier.create(flux)
+				.expectNext("foo")
+				.expectNext("bar")
+				.expectError(RuntimeException.class)
+				.verify();
 	}
 
 //========================================================================================
@@ -48,14 +60,23 @@ public class Part03StepVerifier {
 	// TODO Use StepVerifier to check that the flux parameter emits a User with "swhite"username
 	// and another one with "jpinkman" then completes successfully.
 	void expectSkylerJesseComplete(Flux<User> flux) {
-		fail();
+		StepVerifier.create(flux)
+				.expectNextMatches(user -> user.getUsername().equals("swhite"))
+				//.expectNextMatches(user -> user.getUsername().equals("jpinkman"))
+				// Could use assertNext instead of expectNextMatches, assertNext could be used in widen context
+				.assertNext(user -> assertThat(user.getUsername()).isEqualTo("jpinkman"))
+				.expectComplete()
+				.verify();
 	}
 
 //========================================================================================
 
 	// TODO Expect 10 elements then complete and notice how long the test takes.
 	void expect10Elements(Flux<Long> flux) {
-		fail();
+		StepVerifier.create(flux)
+				.expectNext(0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L)
+				.expectComplete()
+				.verify();
 	}
 
 //========================================================================================
@@ -63,7 +84,10 @@ public class Part03StepVerifier {
 	// TODO Expect 3600 elements at intervals of 1 second, and verify quicker than 3600s
 	// by manipulating virtual time thanks to StepVerifier#withVirtualTime, notice how long the test takes
 	void expect3600Elements(Supplier<Flux<Long>> supplier) {
-		fail();
+		StepVerifier.withVirtualTime(supplier)
+				.thenAwait(Duration.ofHours(1L))
+				.expectNextCount(3600)
+				.verifyComplete();
 	}
 
 	private void fail() {
